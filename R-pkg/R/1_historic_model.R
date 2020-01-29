7853914ef0af5aa08c3f4adf03c229455134679c
GD <gd.dev@libertymail.net>
2020-01-29 13:42:42 +0100

    format simu parameters in panel

--- R-pkg/R/1_historic_model.R
+++ R-pkg/R/1_historic_model.R
@@ -9,17 +9,25 @@ hist_model_ui <- function(add=FALSE) {
                 textAreaInput(
                     "scenario", 
                     label = "Describe your scenario", 
-                    value = "N1 N2\n0 sample 1\n0 sample 2\nt sample 1\nt2 merge 1 2", rows = 10),
+                    value = "N1 N2\n0 sample 1\n0 sample 2\nt sample 1\nt2 merge 1 2", 
+                    rows = 10
+                ),
                 plotOutput("scenario_graph", height = "200px")
             ),
-            verticalLayout(
-                h3("Parameters"),
-                flowLayout(
-                    uiOutput("param_value"),
-                    uiOutput("sample_param"),
+            tabsetPanel(
+                tabPanel(
+                    "Parameters",
+                    uiOutput("param_value")
+                ),
+                tabPanel(
+                    "Sample size",
+                    uiOutput("sample_param")
+                ),
+                tabPanel(
+                    "Simulations",
                     numericInput(
-                        "nsimu", 
-                        label = "Number of repetitions", 
+                        "nsimu",
+                        label = "Number of repetitions",
                         value = 1
                     )
                 )
@@ -46,18 +54,49 @@ hist_model_server <- function(input, output, session) {
     scenario_param <- reactive({
         parse_scenario(input$scenario)
     })
-    
+
     # parameters
     output$param_value <- renderUI({
-        
+
         param_list <- scenario_param()$parameters
         print(paste0(param_list))
-        
+
         # numeric input for each parameters
         numeric_input_list <- list()
         if(length(param_list) > 0) {
             numeric_input_list <- lapply(param_list, function(param) {
                 numericInput(param, label = param, value = 100)
+            })
+        }
+        # Convert the list to a tagList - this is necessary for the list of items
+        # to display properly.
+        do.call(tagList, numeric_input_list)
+    })
+
+    # sample
+    output$sample_param <- renderUI({
+
+        npop <- scenario_param()$npop
+        print(paste0(npop))
+
+        # numeric input for each parameters
+        numeric_input_list <- list()
+        if(!is.null(npop) & npop > 0) {
+            numeric_input_list <- lapply(1:npop, function(pop) {
+                pop_name <- paste0("pop", pop)
+                tagList(
+                    h4(paste0("Population ", pop)),
+                    numericInput(
+                        paste0(pop_name, "_f"),
+                        label = "Nb f.",
+                        value = 25
+                    ),
+                    numericInput(
+                        paste0(pop_name, "_m"),
+                        label = "Nb m.",
+                        value = 25
+                    )
+                )
             })
         }
         # Convert the list to a tagList - this is necessary for the list of items
