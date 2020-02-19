432dd6ff4e3fa459aa9a9e776c7229b583e724fd
GD <gd.dev@libertymail.net>
2020-02-19 10:50:37 +0100

    global import

---
+++ R-pkg/R/diyabcGUI-package.R
@@ -0,0 +1,11 @@
+#' @name diyabcGUI-package
+#' @aliases diyabcGUI
+#' @docType package
+#' @title diyabcGUI
+#' 
+#' diyabcGUI package
+#' 
+#' @author Ghislain Durif
+#' @import shiny
+#' @useDynLib rkeops, .registration = TRUE
+NULL
