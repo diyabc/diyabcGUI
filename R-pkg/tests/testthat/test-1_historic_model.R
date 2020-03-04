9b12cf66c9248faee0436c2bfdb85775400ebd4b
GD <gd.dev@libertymail.net>
2020-03-04 14:32:34 +0100

    work in progress: testing historical model

--- R-pkg/tests/testthat/test-1_historic_model.R
+++ R-pkg/tests/testthat/test-1_historic_model.R
@@ -224,8 +224,14 @@ test_that("parse_event", {
 test_that("parse_scenario", {
     text = "N1 N2\n0 sample 1\n0 sample 2\nt sample 1\nt2 merge 1 2"
     out <- parse_scenario(text)
-    expect_equal(out$npop, 2)
     expect_equal(out$parameters, c("N1", "N2", "t", "t2"))
+    expect_equal(out$npop, 2)
+    # FIXME
+    # expect_equal()
+    # expect_equal()
+    # expect_equal()
+    # expect_equal()
+    # expect_equal()
 })
 
 # text = "N1 N2\n0 sample 1\n0 sample 2\nt sample 110sample 1\nt2 merge 1 2"
