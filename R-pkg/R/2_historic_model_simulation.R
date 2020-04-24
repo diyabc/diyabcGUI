ab767a13e7a1d912c4a9fdc6c19c50bddc82049c
GD <gd.dev@libertymail.net>
2020-04-24 17:04:10 +0200

    fix issue with parameter value choice

--- R-pkg/R/2_historic_model_simulation.R
+++ R-pkg/R/2_historic_model_simulation.R
@@ -42,19 +42,22 @@ hist_model_set_module_server <- function(input, output, session, scenario_def) {
         param_list <- scenario_def$param$Ne_param
         render_model_param(session, param_list,
                            "Ne parameter(s)",
-                           "Effective population size.")
+                           "Effective population size.", 
+                           value = 100, min = 0, max = NA, step = 1)
     })
     
     output$time_param_value <- renderUI({
         param_list <- scenario_def$param$time_param
         render_model_param(session, param_list, "Time parameter(s)",
-                           "Time in <b>generations</b> backward.")
+                           "Time in <b>generations</b> backward.",
+                           value = 100, min = 0, max = NA, step = 1)
     })
     
     output$rate_param_value <- renderUI({
         param_list <- scenario_def$param$rate_param
         render_model_param(session, param_list, "Admixture rate parameter(s)",
-                           "Rate between 0 and 1.")
+                           "Rate between 0 and 1.",
+                           value = 0.5, min = 0, max = 1, step = 0.001)
     })
     
     # sample
@@ -114,7 +117,13 @@ hist_model_module_server <- function(input, output, session) {
 #' Render model parameter ui
 #' @keywords internal
 #' @author Ghislain Durif
-render_model_param <- function(session, param_list, title, doc) {
+#' @param type string, int or float
+#' @param value numeric, default input value. Default is 100.
+#' @param min numeric, input min value. Default is 0.
+#' @param max numeric, input min value. Default is NA (no max value).
+#' @param step numeric, step value for input.
+render_model_param <- function(session, param_list, title, doc, 
+                               value = 100, min = 0, max = NA, step = 1) {
     ns <- session$ns
     
     if(length(param_list) > 0) {
@@ -123,7 +132,9 @@ render_model_param <- function(session, param_list, title, doc) {
             fluidRow(
                 column(
                     width = 10,
-                    numericInput(ns(param), label = param, value = 100, min = 0)
+                    numericInput(ns(param), label = param, 
+                                 value = value, min = min, 
+                                 max = max, step = step)
                 )
             )
         })
