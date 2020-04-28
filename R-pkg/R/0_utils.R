8e1d21171d1ff5c93f03511650ef3c47bd08e07e
GD <gd.dev@libertymail.net>
2020-04-28 11:27:26 +0200

    function to parse existing project folder

--- R-pkg/R/0_utils.R
+++ R-pkg/R/0_utils.R
@@ -48,3 +48,20 @@ help_dir <- function() {
     return(helpdir)
 }
 
+#' Extract project name from path
+#' @keywords internal
+#' @author Ghislain Durif
+#' @importFrom stringr str_split
+#' @importFrom tibble lst
+project_path2name <- function(path) {
+    # check input
+    is_path <- dir.exists(path)
+    # project directory
+    project_name <- tail(str_split(path, .Platform$file.sep)[[1]], 1)
+    # timestamp ?
+    timestamp <- str_detect(project_name, 
+                            pattern = "_[0-9]{4}-[0-9]{2}-[0-9]{2}$")
+    # output
+    return(lst(is_path, project_name, timestamp))
+} 
+
