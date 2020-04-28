2271364eb9a36856c53c0f84ceed0ce09ca59442
GD <gd.dev@libertymail.net>
2020-04-28 11:29:24 +0200

    use module to create or open project

--- R-pkg/R/app_home.R
+++ R-pkg/R/app_home.R
@@ -2,45 +2,41 @@
 #' @keywords internal
 #' @author Ghislain Durif
 #' @importFrom shinydashboard box
+#' @importFrom shinyWidgets actionGroupButtons
 home_module_ui <- function(id, label = "home") {
     ns <- NS(id)
     tagList(
-        h3("Data analysis") %>% 
-            helper(type = "markdown", 
-                   content = "data_analysis"),
-        tags$div(
-            tags$p("Including the two modules of DIYABC Random Forest:"),
-            tags$ul(
-                tags$li("training set simulation"),
-                tags$li("random forest analyses")
+        fluidRow(
+            box(
+                title = "Data analysis",
+                width = 12, 
+                status = "primary", solidHeader = TRUE,
+                collapsible = TRUE,
+                tags$div(
+                    tags$p("Including the two modules of DIYABC Random Forest:"),
+                    tags$ul(
+                        tags$li("training set simulation"),
+                        tags$li("random forest analyses")
+                    )
+                ) %>% 
+                    helper(type = "markdown", 
+                           content = "data_analysis"),
+                project_module_ui(ns("analysis_project"))
             )
         ),
-        fluidPage(
-            actionButton(ns("new_analysis_project"), 
-                         label = "New project", width = '20%')
-        ),
-        tags$br(),
-        fluidPage(
-            actionButton(ns("open_analysis_project"), 
-                         label = "Open project", width = '20%'),
-            dir_input_module_ui(ns("analysis_project_directory")),
-            uiOutput(ns("open_analysis_project_status"))
-        )
-        ,
-        hr(),
-        h4("Pseudo-observed dataset simulation") %>% 
-            helper(type = "markdown", 
-                   content = "data_simulation"),
-        fluidPage(
-            actionButton(ns("new_simu_project"), 
-                         label = "New project", width = '20%')
-        ),
-        tags$br(),
-        fluidPage(
-            actionButton(ns("open_simu_project"), 
-                         label = "Open project", width = '20%'),
-            dir_input_module_ui(ns("simu_project_directory")),
-            uiOutput(ns("open_simu_project_status"))
+        fluidRow(
+            box(
+                title = "Pseudo-observed dataset simulation",
+                width = 12, 
+                status = "info", solidHeader = TRUE,
+                collapsible = TRUE, collapsed = TRUE,
+                tags$div(
+                    tags$p("Direct use of DIYABC-RF simulation engine.") %>% 
+                        helper(type = "markdown", 
+                               content = "data_simulation")
+                ),
+                project_module_ui(ns("simu_project"))
+            )
         )
     )
 }
@@ -49,28 +45,6 @@ home_module_ui <- function(id, label = "home") {
 #' @keywords internal
 #' @author Ghislain Durif
 home_module_server <- function(input, output, session) {
-    analysis_dir <- callModule(dir_input_module_server, 
-                               "analysis_project_directory")
-    simu_dir <- callModule(dir_input_module_server,
-                           "simu_project_directory")
-    
-    # check analysis project directory
-    output$open_analysis_project_status <- renderUI({
-        out <- NULL
-        if(! "header.txt" %in% list.files(analysis_dir$path))
-            out <- tagList(tags$p(
-                    icon("warning"), 
-                    "Chosen directory is not a DIYABC RF analysis directory."))
-        out
-    })
-    
-    # check analysis project directory
-    output$open_simu_project_status <- renderUI({
-        out <- NULL
-        if(! "headersim.txt" %in% list.files(simu_dir$path))
-            out <- tagList(tags$p(
-                icon("warning"), 
-                "Chosen directory is not a DIYABC RF analysis directory."))
-        out
-    })
+    callModule(project_module_server, "analysis_project")
+    callModule(project_module_server, "simu_project")
 }
