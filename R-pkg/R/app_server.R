6c6abc491e3496e7622deb4f93f87e23089d9204
GD <gd.dev@libertymail.net>
2020-05-04 16:06:58 +0200

    temporary deactive shinydashboard

--- R-pkg/R/app_server.R
+++ R-pkg/R/app_server.R
@@ -11,19 +11,6 @@
 #' @importFrom shinyhelper observe_helpers
 #' @return None
 diyabc_server <- function(input, output, session) {
-    # local reactive values
-    local = reactiveValues(
-        analysis_project_list = list(),
-        simu_project_list = list()
-    )
-    # help
-    observe_helpers(session, help_dir = help_dir(), withMathJax = TRUE)
-    # sidebar
-    app_sidebar_update(input, output, session,
-                       analysis_project_list = local$analysis_project_list,
-                       simu_project_list = local$simu_project_list)
-    # body
-    app_body_update(input, output, session)
     # index module
     callModule(index_module_server, "index")
 }
--- R-pkg/R/app_ui.R
+++ R-pkg/R/app_ui.R
@@ -7,9 +7,12 @@
 #' @author Ghislain Durif
 #' @importFrom shinydashboard dashboardPage
 #' @return Shiny ui
-diyabc_ui <- dashboardPage(
-    app_header,
-    app_sidebar,
-    app_body,
-    skin = "black"
+# diyabc_ui <- dashboardPage(
+#     app_header,
+#     app_sidebar,
+#     app_body,
+#     skin = "black"
+# )
+diyabc_ui <- fluidPage(
+    index_module_ui("index")
 )
