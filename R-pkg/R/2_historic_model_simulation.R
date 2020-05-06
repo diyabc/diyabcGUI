3fba8cbd27fa2ae579ace3d17998ba7a303a36eb
GD <gd.dev@libertymail.net>
2020-05-06 13:42:04 +0200

    historic model rendering is ready

--- R-pkg/R/2_historic_model_simulation.R
+++ R-pkg/R/2_historic_model_simulation.R
@@ -3,30 +3,26 @@
 #' @author Ghislain Durif
 hist_model_set_module_ui <- function(id) {
     ns <- NS(id)
-    tagList(
-        navbarPage(
-            "Settings",
-            tabPanel(
-                "Parameters",
-                tagList(
-                    h4("Parameter values"),
-                    tags$hr(),
-                    uiOutput(ns("Ne_param_value")),
-                    uiOutput(ns("time_param_value")),
-                    uiOutput(ns("rate_param_value"))
-                )
-            ),
-            tabPanel(
-                "Samples",
-                uiOutput(ns("sample_param"))
-            ),
-            tabPanel(
-                "Simulations",
-                numericInput(
-                    ns("nsimu"),
-                    label = "Number of repetitions",
-                    value = 1
-                )
+    verticalLayout(
+        tagList(
+            hr(),
+            h3("Parameter values"),
+            uiOutput(ns("Ne_param_value")),
+            uiOutput(ns("time_param_value")),
+            uiOutput(ns("rate_param_value"))
+        ),
+        tagList(
+            hr(),
+            h3("Sample sizes"),
+            uiOutput(ns("sample_param"))
+        ),
+        tagList(
+            hr(),
+            h3("Simulation number"),
+            numericInput(
+                ns("nsimu"),
+                label = "Number of repetitions",
+                value = 1
             )
         )
     )
@@ -38,54 +34,60 @@ hist_model_set_module_ui <- function(id) {
 hist_model_set_module_server <- function(input, output, session, scenario_def) {
     
     # parameters
-    output$Ne_param_value <- renderUI({
-        param_list <- scenario_def$param$Ne_param
-        render_model_param(session, param_list,
-                           "Ne parameter(s)",
-                           "Effective population size.", 
-                           value = 100, min = 0, max = NA, step = 1)
+    observe({
+        output$Ne_param_value <- renderUI({
+            param_list <- scenario_def$scenario_param$Ne_param
+            render_model_param(session, param_list,
+                               "Ne parameter(s)",
+                               "Effective population size.", 
+                               value = 100, min = 0, max = NA, step = 1)
+        })
     })
     
-    output$time_param_value <- renderUI({
-        param_list <- scenario_def$param$time_param
-        render_model_param(session, param_list, "Time parameter(s)",
-                           "Time in <b>generations</b> backward.",
-                           value = 100, min = 0, max = NA, step = 1)
+    observe({
+        output$time_param_value <- renderUI({
+            param_list <- scenario_def$scenario_param$time_param
+            render_model_param(session, param_list, "Time parameter(s)",
+                               "Time in <b>generations</b> backward.",
+                               value = 100, min = 0, max = NA, step = 1)
+        })
     })
     
-    output$rate_param_value <- renderUI({
-        param_list <- scenario_def$param$rate_param
-        render_model_param(session, param_list, "Admixture rate parameter(s)",
-                           "Rate between 0 and 1.",
-                           value = 0.5, min = 0, max = 1, step = 0.001)
+    observe({
+        output$rate_param_value <- renderUI({
+            param_list <- scenario_def$scenario_param$rate_param
+            render_model_param(session, param_list, "Admixture rate parameter(s)",
+                               "Rate between 0 and 1.",
+                               value = 0.5, min = 0, max = 1, step = 0.001)
+        })
     })
     
     # sample
-    output$sample_param <- renderUI({
-        param <- scenario_def$param
-        
-        nsample <- sum(param$event_type == "sample")
-        npop <- param$npop
-
-        sample_pop <- param$event_pop[param$event_type == "sample"]
-        sample_time <- param$event_time[param$event_type == "sample"]
-
-        # numeric input for each sample
-        numeric_input_list <- list()
-        if(!is.null(npop) & nsample > 0) {
-            numeric_input_list <- lapply(1:nsample, function(sample_id) {
-                render_sampling_param(session, sample_id,
-                                      unlist(sample_time[sample_id]),
-                                      unlist(sample_pop[sample_id]))
-            })
-        }
-        # Convert the list to a tagList - this is necessary for the list of items
-        # to display properly.
-        tagList(
-            h4("Sample sizes"),
-            tags$hr(),
-            do.call(flowLayout, numeric_input_list)
-        )
+    observe({
+        output$sample_param <- renderUI({
+            param <- scenario_def$scenario_param
+            
+            nsample <- sum(param$event_type == "sample")
+            npop <- param$npop
+            
+            sample_pop <- param$event_pop[param$event_type == "sample"]
+            sample_time <- param$event_time[param$event_type == "sample"]
+            
+            # numeric input for each sample
+            numeric_input_list <- list()
+            if(!is.null(npop) & nsample > 0) {
+                numeric_input_list <- lapply(1:nsample, function(sample_id) {
+                    render_sampling_param(session, sample_id,
+                                          unlist(sample_time[sample_id]),
+                                          unlist(sample_pop[sample_id]))
+                })
+            }
+            # Convert the list to a tagList - this is necessary for the list of items
+            # to display properly.
+            tagList(
+                do.call(flowLayout, numeric_input_list)
+            )
+        })
     })
 }
 
@@ -109,6 +111,10 @@ hist_model_module_ui <- function(id, label = "hist_model", add=FALSE) {
 hist_model_module_server <- function(input, output, session) {
     
     scenario_def <- callModule(hist_model_def_module_server, "hist_model_def")
+    
+    observe({
+        print(scenario_def)
+    })
     
     callModule(hist_model_set_module_server, "hist_model_set", 
                scenario_def = scenario_def)
