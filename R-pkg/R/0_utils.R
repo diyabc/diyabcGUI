b2aa8c9fef9e946571b9ab046fcd379c1368dfc3
GD <gd.dev@libertymail.net>
2020-05-17 15:35:43 +0200

    function to get binary executable files

--- R-pkg/R/0_utils.R
+++ R-pkg/R/0_utils.R
@@ -1,3 +1,25 @@
+#' Find diyabcGUI related binary files
+#' @keywords internal
+#' @author Ghislain Durif
+find_bin <- function(bin_name = "diyabc") {
+    # check input
+    if(!bin_name %in% c("diyabc", "abcranger")) {
+        stop("Wrong input")
+    }
+    # binary directory
+    path <- bin_dir()
+    # platform
+    os <- str_extract(string = R.version$os, pattern = "windows|darwin|linux")
+    # binary file
+    bin_file <- str_c(bin_name, os, sep = "-")
+    # check if bin file exists
+    if(!bin_file %in% list.files(path)) {
+        stop(str_c("Missing", bin_file, "binary file", sep = " "))
+    }
+    # output
+    return(file.path(path, bin_file))
+}
+
 #' Logging function for debugging
 #' @keywords internal
 #' @author Ghislain Durif
