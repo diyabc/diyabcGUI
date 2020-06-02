0b624a3a90c671e013e056536ef517aedec0f9ed
GD <gd.dev@libertymail.net>
2020-06-02 11:37:55 +0200

    move dashboard content definition to app index file (with related server function)

--- R-pkg/R/app_index.R
+++ R-pkg/R/app_index.R
@@ -1,3 +1,51 @@
+#' App dashboard simplified sidebar
+#' @keywords internal
+#' @author Ghislain Durif
+#' @importFrom shinydashboard dashboardSidebar menuItem sidebarMenu
+app_simplified_sidebar <- function() {
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem(
+                "Home", 
+                tabName = "home_tab", 
+                icon = icon("home")
+            ),
+            menuItem(
+                "Data analysis", 
+                tabName = "analysis_tab", 
+                icon = icon("gear")
+            ),
+            menuItem(
+                "Data simulation", 
+                tabName = "simu_tab", 
+                icon = icon("dna")
+            )
+        )
+    )
+}
+
+#' App dashboard simplified body
+#' @keywords internal
+#' @author Ghislain Durif
+#' @importFrom shinydashboard dashboardBody tabItems tabItem
+#' @importFrom shinyjs useShinyjs
+app_simplified_body <- function() {
+    dashboardBody(
+        useShinyjs(),
+        tabItems(
+            tabItem(tabName = "home_tab",
+                    home_page_ui("home_page")
+            ),
+            tabItem(tabName = "analysis_tab",
+                    analysis_page_ui("analysis_page")
+            ),
+            tabItem(tabName = "simu_tab",
+                    simu_page_ui("simu_page")
+            )
+        )
+    )
+}
+
 #' App simplified dashboard server function
 #' @keywords internal
 #' @author Ghislain Durif
@@ -5,6 +53,28 @@ simplified_index_server <- function(input, output, session) {
     callModule(home_page_server, "home_page")
     callModule(analysis_page_server, "analysis_page")
     callModule(simu_page_server, "simu_page")
+}
+
+#' App dashboard sidebar
+#' @keywords internal
+#' @author Ghislain Durif
+#' @importFrom shinydashboard dashboardSidebar sidebarMenuOutput
+app_sidebar <- function() {
+    dashboardSidebar(
+        sidebarMenuOutput("app_menu")
+    )
+}
+
+#' App dashboard body
+#' @keywords internal
+#' @author Ghislain Durif
+#' @importFrom shinydashboard dashboardBody tabItems tabItem
+#' @importFrom shinyjs useShinyjs
+app_body <- function() {
+    dashboardBody(
+        useShinyjs(),
+        uiOutput("app_tabs")
+    )
 }
 
 #' App dashboard server function
