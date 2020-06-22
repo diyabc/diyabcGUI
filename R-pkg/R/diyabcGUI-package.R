777945609257c9292b43f478eff3a003133cfc36
GD <gd.dev@libertymail.net>
2020-06-22 16:40:15 +0200

    fix import

--- R-pkg/R/diyabcGUI-package.R
+++ R-pkg/R/diyabcGUI-package.R
@@ -8,12 +8,15 @@
 #' @author Ghislain Durif
 #' @importFrom fs path_home dir_delete file_copy file_delete
 #' @importFrom magrittr %>%
+#' @importFrom parallel detectCores mclapply
+#' @importFrom processx process
 #' @import shiny
 #' @importFrom shinybusy add_busy_spinner
 #' @importFrom shinydashboard box
 #' @importFrom shinyhelper helper
 #' @importFrom shinyjs disable enable hidden hide reset show
-#' @importFrom shinyWidgets actionBttn actionGroupButtons radioGroupButtons
-#' @importFrom stringr str_c str_detect str_extract str_extract_all str_length str_match str_replace str_split
+#' @importFrom shinyWidgets actionBttn actionGroupButtons progressBar radioGroupButtons updateProgressBar 
+#' @importFrom stringr str_c str_detect str_extract str_extract_all str_length str_match str_replace str_replace_all str_split str_trim
 #' @importFrom tibble lst
+#' @importFrom tools file_ext
 NULL
