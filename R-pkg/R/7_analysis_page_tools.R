1517ea10b13d1115411aa75d875ae9b89a03e4a9
GD <gd.dev@libertymail.net>
2020-07-03 10:09:54 +0200

    useless file

--- R-pkg/R/7_analysis_page_tools.R
+++
@@ -1,8 +0,0 @@
-#' Write diyabc project file
-#' @keywords internal
-#' @author Ghislain Durif
-write_proj_file <- function(proj_dir, proj_name) {
-    filename <- "diyabcGUI_proj.txt"
-    content <- str_c("project_name=", proj_name)
-    writeLines(content, file.path(proj_dir, filename))
-}
