2ed36a409144d411d476df1ee8e793a7b3b49697
GD <gd.dev@libertymail.net>
2020-07-09 21:46:28 +0200

    build standalone based on electricShine package

---
+++ build_standalone.R
@@ -0,0 +1,32 @@
+# generate standalone interface for Unix
+
+# requirement
+install.packages("devtools")
+install.packages("fs")
+devtools::install_github("chasemc/electricShine")
+
+# install diyabcGUI
+devtools::install("R-pkg")
+
+# build standalone
+build_path <- file.path(getwd(), "build")
+
+# prepare build path
+if(!dir.exists(build_path))
+    fs::dir_create(build_path)
+if(dir.exists(file.path(build_path, "DIYABC-RF")))
+    fs::dir_delete(file.path(build_path, "DIYABC-RF"))
+
+# create standalone app
+electricShine::electrify(
+    app_name = "DIYABC-RF",
+    short_description = "DIYABC-RF application",
+    semantic_version = "1.0.0",
+    build_path = build_path,
+    cran_like_url = "https://cran.r-project.org",
+    function_name = "run_app",
+    local_package_path = file.path(getwd(), "R-pkg"),
+    package_install_opts = list(type = "binary"),
+    run_build = TRUE,
+    permission = TRUE
+)
