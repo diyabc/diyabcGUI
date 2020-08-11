f14b7249b63e1b342778b71770513b63590b9dd6
GD <gd.dev@libertymail.net>
2020-08-11 16:32:03 +0200

    function to get running os name + function to clean diyabc/abcranger binary files + download only diyabc/abcranger binary files related to running os

--- R-pkg/R/0_utils.R
+++ R-pkg/R/0_utils.R
@@ -9,20 +9,7 @@ find_bin <- function(bin_name = "diyabc") {
     # binary directory
     path <- bin_dir()
     # platform
-    os <- str_extract(string = R.version$os, 
-                      pattern = "mingw32|windows|darwin|linux")
-    
-    if(is.na(os)) {
-        stop(str_c("Issue with os:", os, "not supported.", sep = " "))
-    }
-    
-    os_id <- switch(
-        os,
-        "linux"  = "linux",
-        "darwin" = "macos",
-        "mingw32" = "windows",
-        "windows" = "windows",
-    )
+    os_id <- get_os()
     # binary file
     bin_name <- str_c(bin_name, os_id, sep = "-")
     # check if bin file exists
@@ -42,6 +29,44 @@ find_bin <- function(bin_name = "diyabc") {
     return(bin_file)
 }
 
+#' Clean binary directory
+#' @keywords internal
+#' @author Ghislain Durif
+clean_bin_dir <- function() {
+    # bin directory
+    path <- bin_dir()
+    # existing binary file
+    existing_bin_files <- list.files(path)
+    existing_bin_files <- existing_bin_files[str_detect(existing_bin_files, 
+                                                        "diyabc|abcranger")]
+    # delete diyabc/abcranger files
+    if(length(existing_bin_files) > 0) {
+        fs::file_delete(file.path(path, existing_bin_files))
+    }
+}
+
+#' Find which OS is running
+#' @keywords internal
+#' @author Ghislain Durif
+get_os <- function() {
+    # get OS id given by R
+    os_id <- str_extract(string = R.version$os, 
+                         pattern = "mingw32|windows|darwin|linux")
+    # check if error
+    if(is.na(os_id)) {
+        stop(str_c("Issue with os:", os_id, "not supported.", sep = " "))
+    }
+    # return OS name
+    os_name <- switch(
+        os_id,
+        "linux"  = "linux",
+        "darwin" = "macos",
+        "mingw32" = "windows",
+        "windows" = "windows",
+    )
+    return(os_name)
+}
+
 #' Download latest diyabcGUI related binary files if missing
 #' @keywords internal
 #' @author Ghislain Durif
@@ -56,6 +81,9 @@ dl_latest_bin <- function(prog = "diyabc") {
     
     # bin directory
     path <- bin_dir()
+    
+    # platform
+    os_id <- get_os()
     
     # release url
     release_url <- str_c(
@@ -77,22 +105,25 @@ dl_latest_bin <- function(prog = "diyabc") {
     out <- lapply(
         release$browser_download_url, 
         function(single_url) {
-            out <- 0
+            check <- 1
             bin_name <- basename(single_url)
-            if(!bin_name %in% existing_bin_files) {
-                out <- download.file(
-                    single_url, 
-                    destfile = file.path(path, bin_name),
-                    mode = "wb"
-                )
-            } else {
-                warning(str_c(bin_name, "is already the latest release",
-                              sep = " "))
+            if(str_detect(bin_name, os_id)) {
+                if(!bin_name %in% existing_bin_files) {
+                    check <- download.file(
+                        single_url, 
+                        destfile = file.path(path, bin_name),
+                        mode = "wb"
+                    )
+                } else {
+                    check <- 0
+                    warning(str_c(bin_name, "is already the latest release",
+                                  sep = " "))
+                }
             }
-            return(out)
+            return(check)
         }
     )
-    if(!all(out == 0)) {
+    if(!any(out == 0)) {
         stop("Issue with download")
     }
     
