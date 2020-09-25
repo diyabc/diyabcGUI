bfa9082a5be671f612bc86f362e94a22046a786d
GD <gd.dev@libertymail.net>
2020-09-25 11:45:39 +0200

    script to prepare R environment (install dependencies)

---
+++ prepare.R
@@ -0,0 +1,27 @@
+# default repos
+local({
+    r <- getOption("repos")
+    r["CRAN"] <- "https://cran.r-project.org"
+    options(repos=r)
+})
+
+# Dependencies:
+# Ubuntu: apt-get install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
+
+pkg_list <- c(
+    "devtools",
+    "ggplot2",
+    "lubridate",
+    "magrittr",
+    "pbapply",
+    "shiny",
+    "shinyBS",
+    "shinydashboard",
+    "shinyFiles",
+    "shinyhelper",
+    "shinyjs",
+    "shinyWidgets",
+    "stringr",
+    "tibble")
+
+install.packages(pkg_list)
