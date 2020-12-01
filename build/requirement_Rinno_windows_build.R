31596f1d40ce9940c0fa993916b74e366e18d310
GD <gd.dev@libertymail.net>
2020-12-01 11:34:05 +0100

    missing CRAN repos setup

--- build/requirement_Rinno_windows_build.R
+++ build/requirement_Rinno_windows_build.R
@@ -1,3 +1,11 @@
+# default repos
+local({
+    r <- getOption("repos")
+    r["CRAN"] <- "https://cran.r-project.org"
+    options(repos=r)
+})
+
+# install package for RInno standalone build for windows
 pkg_list <- c(
     "devtools", 
     "fs", 
