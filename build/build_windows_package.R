59fc65f6b00af22819ba3469a57263c075a56836
GD <gd.dev@libertymail.net>
2020-12-02 16:33:59 +0100

    improve package building script

--- build/build_windows_package.R
+++ build/build_windows_package.R
@@ -1,5 +1,18 @@
 ## build diyabcGUI zip package for Windows
 
+# default repos
+local({
+    r <- getOption("repos")
+    r["CRAN"] <- "https://cran.r-project.org"
+    options(repos=r)
+})
+
+# requirement
+library(devtools)
+library(fs)
+library(gtools)
+library(stringr)
+
 # project directory
 proj_dir <- system("git rev-parse --show-toplevel", intern = TRUE)
 setwd(proj_dir)
@@ -10,20 +23,9 @@ src_dir <- file.path(proj_dir, "R-pkg")
 # build directory
 build_dir <- file.path(proj_dir, "build")
 
-# local R_lib folder
-R_lib <- file.path(build_dir, "build")
-
-# default repos
-local({
-    r <- getOption("repos")
-    r["CRAN"] <- "https://cran.r-project.org"
-    options(repos=r)
-})
-
-# requirement
-library(devtools)
-library(gtools)
-library(stringr)
+# dist directory
+dist_dir <- file.path(build_dir, "dist")
+if(!dir.exists(dist_dir)) fs::dir_create(dist_dir)
 
 # load diyabcGUI
 devtools::load_all(src_dir)
@@ -37,27 +39,30 @@ dep <- gtools::getDependencies(
 # version
 Version <- as.character(packageVersion("diyabcGUI"))
 
-# local install of dependencies
-install.packages(dep, lib = R_lib)
-
-# local install of package
+# local install of diyabcGUI package
 devtools::install(
     pkg = file.path(proj_dir, "R-pkg"),
-    upgrade = "never",
-    lib = R_lib
+    upgrade = "never"
 )
 
-# # remote install of package
+# # remote install of diyabcGUI package
 # devtools::install_github(
 #     "diyabc/diyabcGUI",
 #     subdir = "R-pkg",
 #     ref = "prod",
-#     upgrade = "never",
-#     lib = R_lib
+#     upgrade = "never"
 # )
 
+# get latest required bin
+library(diyabcGUI)
+diyabcGUI::dl_all_latest_bin()
+
 # zip local install of package
+zipfile <- file.path(dist_dir, str_c("diyabcGUI_", Version, ".zip"))
+if(file.exists(zipfile)) fs::file_delete(zipfile)
+setwd(dirname(system.file(package = "diyabcGUI")))
 zip(
-    zipfile = file.path(build_dir, str_c("diyabcGUI_", Version, ".zip")), 
-    files = file.path(R_lib, "diyabcGUI")
+    zipfile = zipfile, 
+    files = basename(system.file(package = "diyabcGUI"))
 )
+setwd(proj_dir)
