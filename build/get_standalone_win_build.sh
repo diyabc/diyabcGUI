83d27c650223a0c2b593318a6e7a0bd189488361
GD <gd.dev@libertymail.net>
2020-12-03 12:23:13 +0100

    update script to get windows standalone build

--- build/get_standalone_win_build.sh
+++ build/get_standalone_win_build.sh
@@ -8,4 +8,4 @@ if [[ ! -d $DISTDIR ]]; then mkdir -p $DISTDIR; fi
 
 VERSION=$(R -q -s -e "devtools::load_all(\"${PROJDIR}/R-pkg\", quiet=TRUE); cat(as.character(packageVersion(\"diyabcGUI\")))"|tr -d '\n')
 
-scp WinVM:"Documents/diyabcGUI/build/windows/DIYABC-RF_GUI/dist/DIYABC-RF_GUI_Setup_${VERSION}.exe" ${DISTDIR}/DIYABC-RF_GUI_${VERSION}_windows_Setup.exe
+scp WinVM:"Documents/diyabcGUI/build/dist/DIYABC-RF_GUI_${VERSION}_windows.zip" ${DISTDIR}/
