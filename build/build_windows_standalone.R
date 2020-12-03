a93bd81934e1742921d07264bb92d42f18e4f344
GD <gd.dev@libertymail.net>
2020-12-03 12:05:12 +0100

    script to build the DIYABC-RG GUI standalone with DesktopDeployR submodule

---
+++ build/build_windows_standalone.R
@@ -0,0 +1,81 @@
+## build DIYABC-RF_GUI zip standalone for Windows
+
+## check OS
+if(!R.version$os %in% c("mingw32", "windows"))
+    stop("Windows standalone build can only be done on Windows OS")
+
+# default repos
+local({
+    r <- getOption("repos")
+    r["CRAN"] <- "https://cran.r-project.org"
+    options(repos=r)
+})
+
+# requirement
+library(fs)
+library(stringr)
+
+# current working directory
+cwd <- getwd()
+
+# project directory
+proj_dir <- system("git rev-parse --show-toplevel", intern = TRUE)
+
+# package source
+src_dir <- file.path(proj_dir, "R-pkg")
+
+# build directory
+build_dir <- file.path(proj_dir, "build")
+
+# dist directory
+dist_dir <- file.path(build_dir, "dist")
+
+# DesktopDeployR directory
+ddr_dir <- file.path(build_dir, "DesktopDeployR")
+if(!dir.exists(ddr_dir))
+    stop("Missing submodule DesktopDeployR, please init Git submodules")
+
+# standalone name
+app_version <- as.character(packageVersion(
+    "diyabcGUI", lib.loc = file.path(ddr_dir, "app", "library")
+))
+app_name <- "DIYABC-RF_GUI"
+full_app_name <- str(app_name, "_", app_version)
+
+# dependencies
+fs::file_copy(
+    file.path(dist_dir, "requirements.txt"),
+    file.path(ddr_dir, "app", "packages.txt"),
+    overwrite = TRUE
+)
+
+# diyabcGUI windows source
+win_zip <- tail(sort(list.files(dist_dir, pattern = "diyabcGUI*")), 1)
+fs::file_copy(
+    file.path(dist_dir, win_zip),
+    file.path(ddr_dir, "app", "packages.txt"),
+    overwrite = TRUE
+)
+
+# Rcpp sources (temporary fix for R-Portable 4.0.0)
+Rcpp_src <- file.path(ddr_dir, "src", "Rcpp")
+if(!dir.exists(Rcpp_src))
+    system(str_c("git clone https://github.com/RcppCore/Rcpp ", Rcpp_src))
+
+# Environment setup
+setwd(ddr_dir)
+on.exit(setwd(cwd))
+shell.exec("prepare.bat")
+setwd(cwd)
+
+# zip standalone
+zip_dir <- file.path(build_dir, full_app_name)
+file.rename(ddr_dir, zip_dir)
+on.exit(file.rename(zip_dir, ddr_dir))
+setwd(build_dir)
+on.exit(setwd(cwd))
+zipfile <- file.path(dist_dir, str_c(full_app_name, "_windows.zip"))
+zip(
+    zipfile = zipfile, 
+    files = basename(zip_dir)
+)
