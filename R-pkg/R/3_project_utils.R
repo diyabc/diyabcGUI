a10b9c45bcd8e9bac8acaa9806bc1f71cdf7f156
GD <gd.dev@libertymail.net>
2021-01-18 09:33:59 +0100

    rewrite project name module + remote useless project related modules

--- R-pkg/R/3_project_utils.R
+++ R-pkg/R/3_project_utils.R
@@ -1,116 +1,123 @@
-#' New project setting ui
+#' Project naming module ui
 #' @keywords internal
 #' @author Ghislain Durif
-#' @importFrom shinyWidgets actionGroupButtons
-new_proj_set_ui <- function(id) {
+proj_name_ui <- function(id, label = "Project name", default = NULL) {
     ns <- NS(id)
     tagList(
-        proj_name_ui(ns("project_name"), label = "Project",
-                     default = "project_name"),
-        dir_choice_ui(ns("project_dir"), label = "Parent directory"),
-        actionGroupButtons(
-            inputIds = c(ns("validate"), 
-                         ns("reset")),
-            labels = list(
-                tags$span(icon("check"), "Validate"),
-                tags$span(icon("refresh"), "Reset")
+        h3(label),
+        fluidRow(
+            column(
+                width = 9,
+                textInput(
+                    ns("proj_name"), 
+                    label = NULL, 
+                    value = ifelse(
+                        !is.null(default),
+                        default,
+                        ""
+                    ),
+                    placeholder = "project name"
+                )
             ),
-            fullwidth = TRUE
-        )
+            column(
+                width = 3,
+                actionButton(
+                    ns("validate_proj_name"),
+                    label = "Validate",
+                    icon = icon("check"),
+                    width = '100%'
+                )
+            )
+        ),
+        uiOutput(ns("feedback_proj_name"))
     )
 }
 
-#' New project setting server function
+#' Project naming module server
 #' @keywords internal
 #' @author Ghislain Durif
-new_proj_set_server <- function(input, output, session) {
-    # namespace
-    ns <- session$ns
+proj_name_server <- function(input, output, session) {
+    
     # init local
     local <- reactiveValues(
-        enabled = TRUE
+        modified_proj_name = FALSE,
+        valid_proj_name = FALSE
     )
-    # init output reactive values
-    out <- reactiveValues(
-        project_name = NULL,
-        project_dir = NULL,
-        validate = NULL,
-        validation = FALSE,
-        reset = NULL
-    )
-    # project name server side
-    proj_name <- callModule(proj_name_server, "project_name",
-                            enabled = reactive(local$enabled))
-    observeEvent(proj_name$name, {
-        out$project_name <- proj_name$name
+    
+    # init output
+    out <- reactiveValues(proj_name = NULL, valid_proj_name = FALSE)
+    
+    # new input
+    observeEvent(input$proj_name, {
+        shinyjs::enable("validate_proj_name")
+        local$modified_proj_name <- TRUE
+        
+        # check project name
+        local$valid_proj_name <- FALSE
+        if(!is.null(input$proj_name)) {
+            if(str_length(input$proj_name) > 0) {
+                local$valid_proj_name <- TRUE
+            }
+        }
     })
-    # parent folder server side
-    parent_folder <- callModule(dir_choice_server, "project_dir",
-                                enabled = reactive(local$enabled))
-    ## project directory
-    observe({
-        out$project_dir <- file.path(parent_folder$path,
-                                     proj_name$fullname)
+    
+    # validate input
+    observeEvent(input$validate_proj_name, {
+        req(input$validate_proj_name)
+        
+        local$modified_proj_name <- FALSE
+        
+        out$proj_name <- input$proj_name
+        
+        out$valid_proj_name <- local$valid_proj_name
+        
+        shinyjs::disable("validate_proj_name")
     })
-    ## actions
-    observeEvent(input$validate, {
-        local$enabled <- FALSE
-        out$validation <- TRUE
-        out$validate <- ifelse(is.null(out$validate), 0, out$validate) + 1
+    
+    # valid proj name ?
+    observeEvent(local$valid_proj_name, {
+        req(!is.null(local$valid_proj_name))
+        
+        if(!local$valid_proj_name) {
+            shinyjs::enable("validate_proj_name")
+        }
     })
-    observeEvent(input$reset, {
-        local$enabled <- TRUE
-        out$validation <- FALSE
-        out$reset <- ifelse(is.null(out$reset), 0, out$reset) + 1
-    })
-    ## react
-    # validate
-    observeEvent(input$validate, {
-        req(out$project_dir)
-        if(!dir.exists(out$project_dir)) {
-            ret <- dir.create(out$project_dir, recursive = TRUE)
-            if(ret) {
-                showNotification(
-                    id = ns("create_proj_dir_success"),
-                    duration = 5,
-                    closeButton = TRUE,
-                    type = "message",
-                    tagList(
-                        tags$p(
-                            icon("check"),
-                            paste0("Project directory was created.")
-                        )
+    
+    # feedback on project name
+    output$feedback_proj_name <- renderUI({
+        feedback<- tagList()
+        
+        # check if modified proj name
+        if(!is.null(local$modified_proj_name)) {
+            if(local$modified_proj_name) {
+                feedback <- tagAppendChild(
+                    feedback,
+                    helpText(
+                        icon("warning"), 
+                        "Project name is not validated."
                     )
                 )
-            } else {
-                showNotification(
-                    id = ns("create_proj_dir_failed"),
-                    duration = 5,
-                    closeButton = TRUE,
-                    type = "error",
-                    tagList(
-                        tags$p(
-                            icon("warning"),
-                            paste0("Error: project directory was created.")
-                        )
+            }
+        }
+        
+        # check proj name
+        if(!is.null(local$valid_proj_name)) {
+            if(!local$valid_proj_name) {
+                feedback <- tagAppendChild(
+                    feedback,
+                    helpText(
+                        icon("warning"), "Project name is missing."
                     )
                 )
             }
         } else {
-            showNotification(
-                id = ns("proj_dir_exist"),
-                duration = 5,
-                closeButton = TRUE,
-                type = "message",
-                tagList(
-                    tags$p(
-                        icon("check"),
-                        paste0("Project directory already exists.")
-                    )
-                )
-            )
+            
         }
+        
+        # feedback
+        feedback
     })
+    
     # output
     return(out)
 }
