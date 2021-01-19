e457339608aa6e329b40ae680ca5276e333b9870
GD <gd.dev@libertymail.net>
2021-01-19 15:20:21 +0100

    additional imports

--- R-pkg/R/diyabcGUI-package.R
+++ R-pkg/R/diyabcGUI-package.R
@@ -21,7 +21,7 @@
 #' @importFrom shinyhelper helper
 #' @importFrom shinyjs disable enable hidden hide reset show
 #' @importFrom shinyWidgets actionBttn actionGroupButtons ask_confirmation progressBar 
-#' radioGroupButtons updateProgressBar 
+#' radioGroupButtons show_alert updateProgressBar 
 #' @importFrom stringr str_c str_detect str_extract str_extract_all str_length 
 #' str_match str_pad str_replace str_replace_all str_split str_to_upper str_trim
 #' @importFrom tibble lst
