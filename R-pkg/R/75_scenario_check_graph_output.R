b19e46050528ad42ab0e895c5496ebaed7bb9fba
GD <gd.dev@libertymail.net>
2021-02-10 10:23:22 +0100

    refactor scenario coloration in PCA graph + plot couples of PCA components from 1 to 3

--- R-pkg/R/75_scenario_check_graph_output.R
+++ R-pkg/R/75_scenario_check_graph_output.R
@@ -5,10 +5,30 @@
 scenario_check_graph_ouptut <- function(proj_dir, graph_dir, 
                                         prefix = "pcaloc1_") {
     # PCA latent space coordinates
-    g1 <- pca_coordinate_graph(proj_dir, prefix)
+    g1 <- pca_coordinate_graph(proj_dir, prefix, comp = 1:2)
     # save graph
     ggsave(
-        filename = str_c("scenario_check_pca_output_graph.", 
+        filename = str_c("scenario_check_pca_output_graph_1.", 
+                         get_option("image_ext")),
+        plot = g1, 
+        path = graph_dir,
+        units = "cm", width = 14, height = 10
+    )
+    # PCA latent space coordinates
+    g1 <- pca_coordinate_graph(proj_dir, prefix, comp = c(1,3))
+    # save graph
+    ggsave(
+        filename = str_c("scenario_check_pca_output_graph_2.", 
+                         get_option("image_ext")),
+        plot = g1, 
+        path = graph_dir,
+        units = "cm", width = 14, height = 10
+    )
+    # PCA latent space coordinates
+    g1 <- pca_coordinate_graph(proj_dir, prefix, comp = 2:3)
+    # save graph
+    ggsave(
+        filename = str_c("scenario_check_pca_output_graph_3.", 
                          get_option("image_ext")),
         plot = g1, 
         path = graph_dir,
@@ -20,28 +40,46 @@ scenario_check_graph_ouptut <- function(proj_dir, graph_dir,
 #' @keywords internal
 #' @author Ghislain Durif
 #' @importFrom ggplot2 geom_density
-pca_coordinate_graph <- function(proj_dir, prefix = "pcaloc1_") {
+pca_coordinate_graph <- function(proj_dir, prefix = "pcaloc1_", 
+                                 comp = 1:2) {
     # PCA latent space coordinates
     file_name <- file.path(proj_dir, str_c(prefix, "ACP.txt"))
-    pca_coord <- read.csv(file_name, header = FALSE, sep = " ")
-    # preprocessing (remove first line and first two columns)
-    pca_coord <- pca_coord[-1,]
-    pca_coord <- pca_coord[,-c(1,2)]
+    # header
+    header <- unlist(read.csv(file_name, header = FALSE, sep = " ", nrows = 1))
+    # explained variance
+    exp_var <- header[-(1:2)]
+    # file content
+    pca_coord <- read.csv(file_name, header = FALSE, sep = " ", skip = 1)
+    # preprocessing
+    colnames(pca_coord) <- c("id", str_c("comp", 1:(ncol(pca_coord)-1)))
     # tagging
+    pca_coord$id <- as.factor(pca_coord$id)
     pca_coord$tag <- c("observations", 
                        rep("simulations", length = nrow(pca_coord) - 1))
-    # LDA latent space first two axes: observation vs simulation coordinates
+    pca_coord$simulation <- c("observed data",
+                              str_c("scenario", pca_coord$id[-1], sep = " "))
+    # PCA latent space first two axes: observation vs simulation coordinates
     g1 <- ggplot(pca_coord) +
         geom_point(
             data = subset(pca_coord, pca_coord$tag == "simulations"),
-            aes(x=V3, y=V4, col=tag), alpha = 0.1
+            aes_string(x=str_c("comp", comp[1]), 
+                       y=str_c("comp", comp[2]), 
+                       col="simulation"), 
+            alpha = 0.1, size = 1
         ) +
         geom_point(
             data = subset(pca_coord, pca_coord$tag == "observations"),
-            aes(x=V3, y=V4, col=tag), alpha = 1, size = 2
+            aes_string(x=str_c("comp", comp[1]), 
+                       y=str_c("comp", comp[2]), 
+                       col="simulation"), 
+            alpha = 1, size = 2
         ) +
-        xlab("axis 1") +
-        ylab("axis 2") +
+        xlab(str_c("axis", comp[1], 
+                   str_c("(", exp_var[comp[1]] * 100, "%)"), 
+                   sep = " ")) +
+        ylab(str_c("axis", comp[2], 
+                   str_c("(", exp_var[comp[2]] * 100, "%)"), 
+                   sep = " ")) +
         theme_bw(base_size = 12)
     # output
     return(g1)
--- R-pkg/tests/testthat/test-75_scenario_check_graph_output.R
+++ R-pkg/tests/testthat/test-75_scenario_check_graph_output.R
@@ -1,4 +1,37 @@
 test_that("scenario_check_graph_ouptut", {
+    
+    ## Model choice
+    
+    # PoolSeq
+    proj_dir <- file.path(data4test_dir(), "PoolSeq_SNP_model_choice")
+    graph_dir <- mk_proj_dir("testing")
+    prefix <- "pcaloc1_"
+    scenario_check_graph_ouptut(proj_dir, graph_dir, prefix)
+    
+    expect_identical(
+        list.files(graph_dir), 
+        c(
+            str_c("scenario_check_pca_output_graph_", 1:3, ".", 
+                  get_option("image_ext"))
+        )
+    )
+    
+    # IndSeq
+    proj_dir <- file.path(data4test_dir(), "IndSeq_SNP_model_choice")
+    graph_dir <- mk_proj_dir("testing")
+    prefix <- "pcaloc1_"
+    scenario_check_graph_ouptut(proj_dir, graph_dir, prefix)
+    
+    expect_identical(
+        list.files(graph_dir), 
+        c(
+            str_c("scenario_check_pca_output_graph_", 1:3, ".", 
+                  get_option("image_ext"))
+        )
+    )
+    
+    ## Parameter estimation
+    
     # PoolSeq
     proj_dir <- file.path(data4test_dir(), "PoolSeq_SNP_estim_param")
     graph_dir <- mk_proj_dir("testing")
@@ -8,7 +41,7 @@ test_that("scenario_check_graph_ouptut", {
     expect_identical(
         list.files(graph_dir), 
         c(
-            str_c("scenario_check_pca_output_graph.", 
+            str_c("scenario_check_pca_output_graph_", 1:3, ".", 
                   get_option("image_ext"))
         )
     )
@@ -22,7 +55,7 @@ test_that("scenario_check_graph_ouptut", {
     expect_identical(
         list.files(graph_dir), 
         c(
-            str_c("scenario_check_pca_output_graph.", 
+            str_c("scenario_check_pca_output_graph_", 1:3, ".", 
                   get_option("image_ext"))
         )
     )
@@ -30,15 +63,36 @@ test_that("scenario_check_graph_ouptut", {
 
 
 test_that("pca_coordinate_graph", {
+    
+    ## Model choice
+    
+    # PoolSeq
+    proj_dir <- file.path(data4test_dir(), "PoolSeq_SNP_model_choice")
+    prefix <- "pcaloc1_"
+    comp <- 1:2
+    g1 <- pca_coordinate_graph(proj_dir, prefix, comp)
+    expect_true("ggplot" %in% class(g1))
+    
+    # IndSeq
+    proj_dir <- file.path(data4test_dir(), "IndSeq_SNP_model_choice")
+    prefix <- "pcaloc1_"
+    comp <- 1:2
+    g1 <- pca_coordinate_graph(proj_dir, prefix, comp)
+    expect_true("ggplot" %in% class(g1))
+    
+    ## Parameter estimation
+    
     # PoolSeq
     proj_dir <- file.path(data4test_dir(), "PoolSeq_SNP_estim_param")
     prefix <- "pcaloc1_"
-    g1 <- pca_coordinate_graph(proj_dir, prefix)
+    comp <- 1:2
+    g1 <- pca_coordinate_graph(proj_dir, prefix, comp)
     expect_true("ggplot" %in% class(g1))
     
     # IndSeq
     proj_dir <- file.path(data4test_dir(), "IndSeq_SNP_estim_param")
     prefix <- "pcaloc1_"
-    g1 <- pca_coordinate_graph(proj_dir, prefix)
+    comp <- 1:2
+    g1 <- pca_coordinate_graph(proj_dir, prefix, comp)
     expect_true("ggplot" %in% class(g1))
 })
