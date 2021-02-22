505ba6558dea7697c53f389efca0fecf53b3dab3
GD <gd.dev@libertymail.net>
2021-02-22 17:29:43 +0100

    script to update DESCRIPTION file version and date + global version number file

---
+++ build/prebuild.R
@@ -0,0 +1,22 @@
+## pre-build script to update DESCRIPTION file
+# only tested on GNU/Linux
+
+# prebuild function
+prebuild <- function() {
+    ## DESCRIPTION file
+    filename <- file.path(src_dir, "DESCRIPTION")
+    # update Version
+    command <- paste0("sed -i -e ",
+                      "\"s/Version: .*/Version: $(cat ", 
+                      file.path(proj_dir, "version"), ")/\" ",
+                      filename)
+    tmp <- system(command)
+    # update Date
+    command <- paste0("sed -i -e ",
+                      "\"s/Date: .*/Date: ", Sys.Date(), "/\" ",
+                      filename)
+    tmp <- system(command)
+}
+
+# run
+prebuild()
