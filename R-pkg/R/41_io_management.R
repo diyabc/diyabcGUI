d120a0a66b5d09fca92e5a5a347e7337319e97f6
GD <gd.dev@libertymail.net>
2021-04-29 16:47:10 +0200

    parse and check statobs file (+test)

--- R-pkg/R/41_io_management.R
+++ R-pkg/R/41_io_management.R
@@ -1729,26 +1729,65 @@ check_header_loci_des <- function(strng, type = "mss") {
 parse_diyabc_statobs <- function(file_name, file_type, n_stat) {
     
     # init output
-    out <- list(msg = list(), valid = FALSE)
+    out <- list(msg = list(), valid = TRUE)
     
     # check file_name
     tmp <- check_file_name(file_name)
+    if(!tmp) {
+        out$valid <- FALSE
+        msg <- tagList("Invalid file file.")
+        out$msg <- append(out$msg, list(msg))
+    }
     
     # check file_type
     if(file_type != "text/plain") {
         out$valid <- FALSE
         msg <- tagList("Wrong file type.")
         out$msg <- append(out$msg, list(msg))
+    }
+    
+    # continue ?
+    if(!out$valid) {
         return(out)
     }
     
-    
-    
     # try reading it
     tmp <- tryCatch(
-        read.table(file_name)
+        read.table(file_name), 
+        error = function(e) return(NULL)
     )
     
+    if(is.null(tmp)) {
+        out$valid <- FALSE
+        msg <- tagList("Issue with statobs file format.")
+        out$msg <- append(out$msg, list(msg))
+        return(out)
+    }
+    
+    # check number of rows and columns
+    if(nrow(tmp) != 2) {
+        out$valid <- FALSE
+        msg <- tagList(
+            "The statobs file is supposed to contain two lines:",
+            "one with the names of the summary statistics,",
+            "and a second with the corresponding values",
+            "computed on the dataset."
+        )
+        out$msg <- append(out$msg, list(msg))
+    }
+    
+    if(ncol(tmp) != n_stat) {
+        out$valid <- FALSE
+        msg <- tagList(
+            "The number of summary statistics in the statobs file",
+            "does not correspond to the number of summary statistics",
+            "in the reftable file."
+        )
+        out$msg <- append(out$msg, list(msg))
+    }
+    
+    # output
+    return(out)
 }
 
 #' Parse diyabc simulation header file
--- R-pkg/tests/testthat/test-41_io_management.R
+++ R-pkg/tests/testthat/test-41_io_management.R
@@ -370,3 +370,36 @@ test_that("parse_diyabc_header_scenarii", {
                  list(id=NULL, n_param=NULL, prior=NULL, raw_scenario=NULL,
                       valid=FALSE))
 })
+
+test_that("parse_diyabc_statobs", {
+    
+    # existing file
+    test_proj <- "IndSeq_SNP_estim_param"
+    test_dir <- file.path(data4test_dir(), test_proj)
+    file_name <- file.path(test_dir, "statobsRF.txt")
+    file_type <- "text/plain"
+    tmp <- parse_diyabc_statobs(file_name, file_type, n_stat = 130)
+    expect_true(tmp$valid)
+    
+    # bad number of summary stats
+    test_proj <- "IndSeq_SNP_estim_param"
+    test_dir <- file.path(data4test_dir(), test_proj)
+    file_name <- file.path(test_dir, "statobsRF.txt")
+    file_type <- "text/plain"
+    res <- parse_diyabc_statobs(file_name, file_type, n_stat = 120)
+    expect_false(res$valid)
+    expect_true(length(res$msg) == 1)
+    
+    # unexisting file
+    file_name <- file.path(test_dir, "toto.txt")
+    tmp <- parse_diyabc_statobs(file_name, file_type, n_stat = 130)
+    expect_false(tmp$valid)
+    expect_true(length(tmp$msg) == 1)
+    
+    # unexisting file and wrong file type
+    file_name <- file.path(test_dir, "toto.txt")
+    file_type <- "toto"
+    tmp <- parse_diyabc_statobs(file_name, file_type, n_stat = 130)
+    expect_false(tmp$valid)
+    expect_true(length(tmp$msg) == 2)
+})
