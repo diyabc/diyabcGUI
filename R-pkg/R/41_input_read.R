3619f1d691683292d24638c763634ff7ae23c0fb
GD <gd.dev@libertymail.net>
2021-04-30 19:14:47 +0200

    implement header reader and related functions

--- R-pkg/R/41_input_read.R
+++ R-pkg/R/41_input_read.R
@@ -111,16 +111,18 @@
 #' Content: see doc
 #' @param file_name string, (server-side) path to a headersim file.
 #' @param file_type string, MIME file type.
+#' @param locus_type string, "snp" or "mss"
 #' @importFrom readr read_file
-read_header <- function(file_name, file_type) {
+read_header <- function(file_name, file_type, locus_type = "snp") {
     
     # init output
     out <- list(
         msg = list(), valid = TRUE,
         data_file = NULL, loci_desc = NULL, 
         n_param = NULL, n_prior = NULL, n_sumstat = NULL, 
-        cond_list = NULL, prior_list = NULL, group_prior_list = NULL, 
-        n_scenario = NULL, scenario_list = NULL, 
+        cond_list = NULL, prior_list = NULL, 
+        n_group = NULL, group_prior_list = NULL, 
+        n_scenario = NULL, scenario_list = NULL,
         simu_mode = NULL
     )
     
@@ -156,10 +158,10 @@ read_header <- function(file_name, file_type) {
     current_line <- current_line + 1
     
     ## number of parameters and statistics
+    pttrn <- "^[0-9]+ parameters and [0-9]+ summary statistics$"
     strng <- header[1]
     header <- header[-1]
     current_line <- current_line + 1
-    pttrn <- "^[0-9]+ parameters and [0-9]+ summary statistics$"
     if(!str_detect(strng, pttrn)) {
         out$valid <- FALSE
         msg <- tagList(
@@ -167,12 +169,13 @@ read_header <- function(file_name, file_type) {
             "that should match the pattern", tags$code(pttrn), "."
         )
         out$msg <- append(out$msg, list(msg))
-    } else {
-        pttrn <- "[0-9]+(?= parameters)"
-        out$n_param <- as.integer(str_extract(strng, pttrn))
-        pttrn <- "[0-9]+(?= summary statistics)"
-        out$n_sumstat <- as.integer(str_extract(strng, pttrn))
+        return(out)
     }
+    
+    pttrn <- "[0-9]+(?= parameters)"
+    out$n_param <- as.integer(str_extract(strng, pttrn))
+    pttrn <- "[0-9]+(?= summary statistics)"
+    out$n_sumstat <- as.integer(str_extract(strng, pttrn))
     
     ## empty line
     strng <- header[1]
@@ -185,6 +188,7 @@ read_header <- function(file_name, file_type) {
             "that should be empty."
         )
         out$msg <- append(out$msg, list(msg))
+        return(out)
     }
     
     ## scenarios
@@ -200,36 +204,313 @@ read_header <- function(file_name, file_type) {
             "that should match the pattern", tags$code(pttrn), "."
         )
         out$msg <- append(out$msg, list(msg))
-    } else {
-        pttrn <- "[0-9]+(?= scenarios:)"
-        out$n_scenario <- as.integer(str_extract(strng, pttrn))
-        pttrn <- "(?<= )[0-9]+"
-        nrow_per_scenario <- as.integer(unlist(str_extract_all(strng, pttrn)))
-        # extract scenarii
-        row_seq <- cumsum(c(1, nrow_per_scenario+1))
-        scenario_list <- lapply(
-            split(
-                raw_content[(min(line_seq):(max(line_seq)-1)) 
-                            + next_sec_line], 
-                rep(seq(line_seq), diff(c(line_seq, max(line_seq))))
-            ), 
-            function(content) {
-                parse_diyabc_header_scenarii(content)
-            })
-        # check extracted scnenarii
-        valid <- all(unlist(lapply(scenario_list, 
-                                   function(item) item$valid)))
-        # extract raw scenario list
-        raw_scenario_list <- unlist(unname(
-            lapply(
-                scenario_list, 
-                function(item) item$raw_scenario
-            )
-        ))
-        # next section
-        next_sec_line <- next_sec_line + max(line_seq)
+        return(out)
     }
     
+    ## scenario config
+    pttrn <- "[0-9]+(?= scenarios:)"
+    out$n_scenario <- as.integer(str_extract(strng, pttrn))
+    pttrn <- "(?<= )[0-9]+"
+    nrow_per_scenario <- as.integer(unlist(str_extract_all(strng, pttrn)))
+    ## extract scenarii
+    row_seq <- cumsum(c(1, nrow_per_scenario+1))
+    parsed_scenario_list <- lapply(
+        split(
+            header[(min(row_seq):(max(row_seq)-1))], 
+            rep(seq(row_seq), diff(c(row_seq, max(row_seq))))
+        ), 
+        function(content) parse_header_scenario(content)
+    )
+    # next
+    header <- header[-(1:(max(row_seq)-1))]
+    current_line <- current_line + max(row_seq) - 1
+    # check extracted scnenarii
+    scen_check <- unlist(lapply(
+        parsed_scenario_list, function(item) item$valid
+    ))
+    scen_id <- unlist(lapply(
+        parsed_scenario_list, function(item) item$id
+    ))
+    if(!all(scen_check)) {
+        out$valid <- FALSE
+        msg <- tagList(
+            "Issue with format of following scenarii:", 
+            str_c(scen_id[!scen_check], collapse = ", "), "."
+        )
+        out$msg <- append(out$msg, list(msg))
+        return(out)
+    }
+    # extract raw scenario list
+    out$scenario_list <- unlist(unname(lapply(
+        parsed_scenario_list, 
+        function(item) item$scenario
+    )))
+    
+    ## empty line
+    strng <- header[1]
+    header <- header[-1]
+    current_line <- current_line + 1
+    if(strng != "") {
+        out$valid <- FALSE
+        msg <- tagList(
+            "Issue with line", tags$b(as.character(current_line)), 
+            "that should be empty."
+        )
+        out$msg <- append(out$msg, list(msg))
+        return(out)
+    }
+    
+    ## historical parameters
+    pttrn <- "^historical parameters priors \\([0-9]+,[0-9]+\\)$"
+    strng <- header[1]
+    header <- header[-1]
+    current_line <- current_line + 1
+    # find section
+    if(!any(str_detect(strng, pttrn))) {
+        out$valid <- FALSE
+        msg <- tagList(
+            "Issue with line", tags$b(as.character(current_line)), 
+            "that should match the pattern", tags$code(pttrn), "."
+        )
+        out$msg <- append(out$msg, list(msg))
+        return(out)
+    }
+    
+    # number of priors
+    pttrn <- "(?<= \\()[0-9]+"
+    out$n_prior <- as.integer(str_extract(strng, pttrn))
+    # number of conditions
+    pttrn <- "[0-9]+(?=\\)$)"
+    out$n_cond <- as.integer(str_extract(strng, pttrn))
+    
+    ## parameter config
+    # extract priors
+    out$prior_list <- header[1:out$n_prior]
+    header <- header[-(1:out$n_prior)]
+    current_line <- current_line + out$n_prior
+    # check extracted priors
+    prior_check <- unlist(lapply(out$prior_list, check_header_prior))
+    if(!all(prior_check)) {
+        out$valid <- FALSE
+        msg <- tagList(
+            "Issue with format of parameter priors at lines:", 
+            str_c(which(!prior_check) + current_line, collapse = ", "), 
+            "."
+        )
+        out$msg <- append(out$msg, list(msg))
+        return(out)
+    }
+    
+    # extract conditions
+    if(out$n_cond > 0) {
+        
+        out$cond_list <- header[1:out$n_cond]
+        header <- header[-(1:out$n_cond)]
+        current_line <- current_line + out$n_cond
+        # check extracted conds
+        cond_check <- unlist(lapply(out$cond_list, check_header_cond))
+        if(!all(cond_check)) {
+            out$valid <- FALSE
+            msg <- tagList(
+                "Issue with format of parameter conditions at lines:", 
+                str_c(which(!cond_check) + current_line, collapse = ", "), 
+                "."
+            )
+            out$msg <- append(out$msg, list(msg))
+            return(out)
+        }
+        
+        # generation mode
+        pttrn <- "DRAW UNTIL"
+        out$simu_mode <- header[1]
+        header <- header[-1]
+        current_line <- current_line + 1
+        if(out$simu_mode != pttrn) {
+            out$valid <- FALSE
+            msg <- tagList(
+                "Missing 'DRAW UNTIL' after conditions at lines:", 
+                as.character(current_line+1), 
+                "."
+            )
+            out$msg <- append(out$msg, list(msg))
+            return(out)
+        }
+        
+    }
+    
+    ## empty line (and check for unnecessary simulation mode)
+    strng <- header[1]
+    header <- header[-1]
+    current_line <- current_line + 1
+    if(strng != "") {
+        if(strng == "DRAW UNTIL") {
+            out$valid <- FALSE
+            msg <- tagList(
+                "Issue with line", tags$b(as.character(current_line)), 
+                "unnecessary 'DRAW UNTIL'."
+            )
+            out$msg <- append(out$msg, list(msg))
+        } else {
+            out$valid <- FALSE
+            msg <- tagList(
+                "Issue with line", tags$b(as.character(current_line)), 
+                "that should be empty."
+            )
+            out$msg <- append(out$msg, list(msg))
+        }
+        return(out)
+    }
+    
+    ## loci description
+    pttrn <- "^loci description \\([0-9]+\\)$"
+    strng <- header[1]
+    header <- header[-1]
+    current_line <- current_line + 1
+    # find section
+    if(!any(str_detect(strng, pttrn))) {
+        out$valid <- FALSE
+        msg <- tagList(
+            "Issue with line", tags$b(as.character(current_line)), 
+            "that should match the pattern", tags$code(pttrn), "."
+        )
+        out$msg <- append(out$msg, list(msg))
+        return(out)
+    }
+    
+    # number of loci description
+    pttrn <- "(?<=^loci description \\()[0-9]+"
+    out$n_loci_desc <- as.integer(str_extract(strng, pttrn))
+    
+    # extract loci description
+    out$loci_desc <- header[1:out$n_loci_desc]
+    header <- header[-(1:out$n_loci_desc)]
+    current_line <- current_line + out$n_loci_desc
+    # check extracted loci description
+    loci_desc_check <- unlist(lapply(
+        out$loci_desc, 
+        check_header_loci_desc, type = locus_type
+    ))
+    if(!all(loci_desc_check)) {
+        out$valid <- FALSE
+        msg <- tagList(
+            "Issue with format of locus description at lines:", 
+            str_c(which(!loci_desc_check) + current_line, collapse = ", "), 
+            "."
+        )
+        out$msg <- append(out$msg, list(msg))
+        return(out)
+    }
+    
+    ## empty line
+    strng <- header[1]
+    header <- header[-1]
+    current_line <- current_line + 1
+    if(strng != "") {
+        out$valid <- FALSE
+        msg <- tagList(
+            "Issue with line", tags$b(as.character(current_line)), 
+            "that should be empty."
+        )
+        out$msg <- append(out$msg, list(msg))
+        return(out)
+    }
+    
+    ## group prior (for microsat/sequence)
+    if(locus_type == "mss") {
+        
+        pttrn <- "^group priors \\([0-9]+\\)$"
+        strng <- header[1]
+        header <- header[-1]
+        current_line <- current_line + 1
+        # find section
+        if(!any(str_detect(strng, pttrn))) {
+            out$valid <- FALSE
+            msg <- tagList(
+                "Issue with line", tags$b(as.character(current_line)), 
+                "that should match the pattern", tags$code(pttrn), "."
+            )
+            out$msg <- append(out$msg, list(msg))
+            return(out)
+        }
+        
+        # number of group prior
+        pttrn <- "(?<=^group priors \\()[0-9]"
+        out$n_group <- as.integer(str_extract(strng, pttrn))
+        
+        # find next section
+        tmp_current_line <- current_line
+        end_sec <- head(which(header == ""), 1)
+        content <- header[1:(end_sec-1)]
+        header <- header[-(1:(end_sec-1))]
+        current_line <- current_line + end_sec - 1
+        
+        # extract group prior
+        check_group_prior <- parse_header_group_prior(
+            content, out$n_group, tmp_current_line
+        )
+        
+        if(!check_group_prior$valid) {
+            out$valid <- FALSE
+            msg <- tagList(
+                "Issue with format of group prior around line ", 
+                tags$b(as.character(check_group_prior$current_line)), 
+                "."
+            )
+            out$msg <- append(out$msg, list(msg))
+            return(out)
+        }
+        
+        current_line <- check_group_prior$current_line
+        out$group_prior_list <- check_group_prior$group_prior
+        
+        ## empty line
+        strng <- header[1]
+        header <- header[-1]
+        current_line <- current_line + 1
+        if(strng != "") {
+            out$valid <- FALSE
+            msg <- tagList(
+                "Issue with line", tags$b(as.character(current_line)), 
+                "that should be empty."
+            )
+            out$msg <- append(out$msg, list(msg))
+            return(out)
+        }
+    }
+    
+    ## group summary statistics
+    pttrn <- "^group summary statistics \\([0-9]+\\)$"
+    strng <- header[1]
+    header <- header[-1]
+    current_line <- current_line + 1
+    # find section
+    if(!any(str_detect(strng, pttrn))) {
+        out$valid <- FALSE
+        msg <- tagList(
+            "Issue with line", tags$b(as.character(current_line)), 
+            "that should match the pattern", tags$code(pttrn), "."
+        )
+        out$msg <- append(out$msg, list(msg))
+        return(out)
+    }
+    
+    # number of summary stats
+    pttrn <- "(?<=^group summary statistics \\()[0-9]+"
+    tmp_n_sumstat <- sum(as.integer(str_extract(strng, pttrn)), na.rm = TRUE)
+    # check
+    if(out$n_sumstat != tmp_n_sumstat) {
+        out$valid <- FALSE
+        msg <- tagList(
+            "Issue with line", tags$b(as.character(current_line)), 
+            "the number of summary statistics is different",
+            "from the number given in line 2."
+        )
+        out$msg <- append(out$msg, list(msg))
+        return(out)
+    }
+    
+    ##
+    # TODO: parse end of file
     
     # output
     return(out)
@@ -242,7 +523,7 @@ read_header <- function(file_name, file_type) {
 #' @description
 #' Content: see doc
 #' @param content string vector, scenario description
-parse_scenario_header <- function(content) {
+parse_header_scenario <- function(content) {
     
     # init output
     out <- list(
@@ -269,6 +550,62 @@ parse_scenario_header <- function(content) {
         ## scenario
         out$scenario <- str_c(content[-1], collapse = "\n")
     }
+    ## output
+    return(out)
+}
+
+
+#' Parse scenario in header file
+#' @keywords internal
+#' @author Ghislain Durif
+#' @description
+#' Content: see doc
+#' @param content string vector, scenario description
+parse_header_group_prior <- function(content, n_group, current_line) {
+    
+    # init output
+    out <- list(
+        valid = FALSE, ind = NULL, group_prior = NULL, mss_type = NULL,
+        current_line = current_line
+    )
+    
+    # loop over group priors
+    for(ind in 1:n_group) {
+        # which group
+        out$ind <- ind
+        # description (1st line)
+        pttrn <- str_c("^group G[0-9]+ \\[(M|S)\\]$")
+        strng <- content[1]
+        content <- content[-1]
+        out$current_line <- out$current_line + 1
+        if(!str_detect(strng, pttrn)) {
+            out$valid <- FALSE
+            return(out)
+        } else {
+            group_prior_head <- strng
+            # locus type
+            pttrn <- "(?<=\\[)(M|S)(?=\\])"
+            out$mss_type[ind] <- str_extract(strng, pttrn)
+            n_line <- switch (
+                out$mss_type[ind],
+                "M" = 6,
+                "S" = 7,
+                NA
+            )
+            out$valid <- check_header_group_prior(
+                head(content, n_line), type = out$mss_type[ind]
+            )
+            if(!out$valid) {
+                return(out)
+            }
+            out$group_prior[ind] <- str_c(
+                c(group_prior_head, head(content, n_line)), collapse = "\n"
+            )
+            content <- content[-(1:n_line)]
+            out$current_line <- out$current_line + n_line
+        }
+    }
+    
     ## output
     return(out)
 }
--- R-pkg/tests/testthat/test-41_input_read.R
+++ R-pkg/tests/testthat/test-41_input_read.R
@@ -15,45 +15,203 @@ context("41_input_read")
 
 test_that("read_header", {
     
-    # existing file
+    ## SNP IndSeq
+    # estim param
     test_proj <- "IndSeq_SNP_estim_param"
     test_dir <- file.path(data4test_dir(), test_proj)
     file_name <- file.path(test_dir, "headerRF.txt")
     file_type <- "text/plain"
-    tmp <- read_statobs(file_name, file_type, n_stat = 130)
-    expect_true(tmp$valid)
+    locus_type = "snp"
+    res <- read_header(file_name, file_type, locus_type)
+    expect_true(res$valid)
+    expect_equal(length(res$msg), 0)
+    expect_equal(res$data_file, "indseq_SNP_sim_dataset_4POP_001.snp")
+    expect_equal(res$n_param, 8)
+    expect_equal(res$n_sumstat, 130)
+    expect_equal(res$n_scenario, 1)
+    expect_equal(res$n_scenario, length(res$scenario_list))
+    expect_equal(res$n_cond, 2)
+    expect_equal(res$n_cond, length(res$cond_list))
+    expect_equal(res$simu_mode, "DRAW UNTIL")
+    expect_equal(res$n_loci_desc, 1)
+    expect_equal(res$n_loci_desc, length(res$loci_desc))
+    expect_equal(res$n_prior, 8)
+    expect_equal(res$n_prior, length(res$prior_list))
     
-    # bad number of summary stats
-    test_proj <- "IndSeq_SNP_estim_param"
-    test_dir <- file.path(data4test_dir(), test_proj)
-    file_name <- file.path(test_dir, "statobsRF.txt")
+    # indeseq -> bad header file
+    test_proj <- "bad_files"
+    test_dir <- file.path(test_input_dir(), "bad_files")
+    file_name <- file.path(test_dir, "headerRF.txt")
     file_type <- "text/plain"
-    res <- read_statobs(file_name, file_type, n_stat = 120)
+    locus_type = "mss"
+    res <- read_header(file_name, file_type, locus_type)
     expect_false(res$valid)
-    expect_true(length(res$msg) == 1)
+    expect_equal(length(res$msg), 1)
     
-    # unexisting file
-    file_name <- file.path(test_dir, "toto.txt")
-    tmp <- read_statobs(file_name, file_type, n_stat = 130)
-    expect_false(tmp$valid)
-    expect_true(length(tmp$msg) == 1)
-    
-    # unexisting file and wrong file type
-    file_name <- file.path(test_dir, "toto.txt")
-    file_type <- "toto"
-    tmp <- read_statobs(file_name, file_type, n_stat = 130)
-    expect_false(tmp$valid)
-    expect_true(length(tmp$msg) == 2)
-})
-
-test_that("parse_scenario_header", {
     # model choice
     test_proj <- "IndSeq_SNP_model_choice"
     test_dir <- file.path(data4test_dir(), test_proj)
     file_name <- file.path(test_dir, "headerRF.txt")
-    content <- str_split(read_file(file_name), "\n", simplify = TRUE)
+    file_type <- "text/plain"
+    locus_type = "snp"
+    res <- read_header(file_name, file_type, locus_type)
+    expect_true(res$valid)
+    expect_equal(length(res$msg), 0)
+    expect_equal(res$data_file, "indseq_SNP_sim_dataset_4POP_001.snp")
+    expect_equal(res$n_param, 13)
+    expect_equal(res$n_sumstat, 130)
+    expect_equal(res$n_scenario, 6)
+    expect_equal(res$n_scenario, length(res$scenario_list))
+    expect_equal(res$n_cond, 6)
+    expect_equal(res$n_cond, length(res$cond_list))
+    expect_equal(res$simu_mode, "DRAW UNTIL")
+    expect_equal(res$n_loci_desc, 1)
+    expect_equal(res$n_loci_desc, length(res$loci_desc))
+    expect_equal(res$n_prior, 13)
+    expect_equal(res$n_prior, length(res$prior_list))
     
-    res <- parse_scenario_header(content[5:13])
+    ## SNP PoolSeq
+    # estim param
+    test_proj <- "PoolSeq_SNP_estim_param"
+    test_dir <- file.path(data4test_dir(), test_proj)
+    file_name <- file.path(test_dir, "headerRF.txt")
+    file_type <- "text/plain"
+    locus_type = "snp"
+    res <- read_header(file_name, file_type, locus_type)
+    expect_true(res$valid)
+    expect_equal(length(res$msg), 0)
+    expect_equal(res$data_file, "poolseq_SNP_sim_dataset_4POP_cov100_001.snp")
+    expect_equal(res$n_param, 8)
+    expect_equal(res$n_sumstat, 130)
+    expect_equal(res$n_scenario, 1)
+    expect_equal(res$n_scenario, length(res$scenario_list))
+    expect_equal(res$n_cond, 2)
+    expect_equal(res$n_cond, length(res$cond_list))
+    expect_equal(res$simu_mode, "DRAW UNTIL")
+    expect_equal(res$n_loci_desc, 1)
+    expect_equal(res$n_loci_desc, length(res$loci_desc))
+    expect_equal(res$n_prior, 8)
+    expect_equal(res$n_prior, length(res$prior_list))
+    
+    # model choice
+    test_proj <- "PoolSeq_SNP_model_choice"
+    test_dir <- file.path(data4test_dir(), test_proj)
+    file_name <- file.path(test_dir, "headerRF.txt")
+    file_type <- "text/plain"
+    locus_type = "snp"
+    res <- read_header(file_name, file_type, locus_type)
+    expect_true(res$valid)
+    expect_equal(length(res$msg), 0)
+    expect_equal(res$data_file, "poolseq_SNP_sim_dataset_4POP_cov100_001.snp")
+    expect_equal(res$n_param, 13)
+    expect_equal(res$n_sumstat, 130)
+    expect_equal(res$n_scenario, 6)
+    expect_equal(res$n_scenario, length(res$scenario_list))
+    expect_equal(res$n_cond, 6)
+    expect_equal(res$n_cond, length(res$cond_list))
+    expect_equal(res$simu_mode, "DRAW UNTIL")
+    expect_equal(res$n_loci_desc, 1)
+    expect_equal(res$n_loci_desc, length(res$loci_desc))
+    expect_equal(res$n_prior, 13)
+    expect_equal(res$n_prior, length(res$prior_list))
+    
+    ## MSS
+    # microsat
+    test_proj <- "Microsat"
+    test_dir <- file.path(data4test_dir(), test_proj)
+    file_name <- file.path(test_dir, "header.txt")
+    file_type <- "text/plain"
+    locus_type = "mss"
+    res <- read_header(file_name, file_type, locus_type)
+    expect_true(res$valid)
+    expect_equal(length(res$msg), 0)
+    expect_equal(
+        res$data_file, 
+        "simu_dataset_microsat_one_pop_bottleneck_multisamples_001.mss"
+    )
+    expect_equal(res$n_param, 6)
+    expect_equal(res$n_sumstat, 40)
+    expect_equal(res$n_scenario, 2)
+    expect_equal(res$n_scenario, length(res$scenario_list))
+    expect_equal(res$n_cond, 0)
+    expect_equal(res$n_cond, length(res$cond_list))
+    expect_null(res$simu_mode)
+    expect_equal(res$n_loci_desc, 50)
+    expect_equal(res$n_loci_desc, length(res$loci_desc))
+    expect_equal(res$n_prior, 3)
+    expect_equal(res$n_prior, length(res$prior_list))
+    expect_equal(res$n_group, 1)
+    expect_equal(res$n_group, length(res$group_prior_list))
+    
+    # microsat sequence 1
+    test_proj <- "Microsat_Sequences"
+    test_dir <- file.path(data4test_dir(), test_proj)
+    file_name <- file.path(test_dir, "header.txt")
+    file_type <- "text/plain"
+    locus_type = "mss"
+    res <- read_header(file_name, file_type, locus_type)
+    expect_true(res$valid)
+    expect_equal(length(res$msg), 0)
+    expect_equal(res$data_file, "mss_example_001.mss")
+    expect_equal(res$n_param, 9)
+    expect_equal(res$n_sumstat, 2)
+    expect_equal(res$n_scenario, 1)
+    expect_equal(res$n_scenario, length(res$scenario_list))
+    expect_equal(res$n_cond, 2)
+    expect_equal(res$n_cond, length(res$cond_list))
+    expect_equal(res$simu_mode, "DRAW UNTIL")
+    expect_equal(res$n_loci_desc, 28)
+    expect_equal(res$n_loci_desc, length(res$loci_desc))
+    expect_equal(res$n_prior, 9)
+    expect_equal(res$n_prior, length(res$prior_list))
+    expect_equal(res$n_group, 2)
+    expect_equal(res$n_group, length(res$group_prior_list))
+    
+    # microsat sequence 1 -> bad header file
+    test_proj <- "bad_files"
+    test_dir <- file.path(test_input_dir(), "bad_files")
+    file_name <- file.path(test_dir, "header.txt")
+    file_type <- "text/plain"
+    locus_type = "mss"
+    res <- read_header(file_name, file_type, locus_type)
+    expect_false(res$valid)
+    expect_equal(length(res$msg), 1)
+    
+    # microsat sequence 2
+    test_proj <- "Microsat_Sequences2"
+    test_dir <- file.path(data4test_dir(), test_proj)
+    file_name <- file.path(test_dir, "header.txt")
+    file_type <- "text/plain"
+    locus_type = "mss"
+    res <- read_header(file_name, file_type, locus_type)
+    expect_true(res$valid)
+    expect_equal(length(res$msg), 0)
+    expect_equal(res$data_file, "toytest2_micro_seq_complexe_001.mss")
+    expect_equal(res$n_param, 22)
+    expect_equal(res$n_sumstat, 85)
+    expect_equal(res$n_scenario, 2)
+    expect_equal(res$n_scenario, length(res$scenario_list))
+    expect_equal(res$n_cond, 2)
+    expect_equal(res$n_cond, length(res$cond_list))
+    expect_equal(res$simu_mode, "DRAW UNTIL")
+    expect_equal(res$n_loci_desc, 28)
+    expect_equal(res$n_loci_desc, length(res$loci_desc))
+    expect_equal(res$n_prior, 9)
+    expect_equal(res$n_prior, length(res$prior_list))
+    expect_equal(res$n_group, 5)
+    expect_equal(res$n_group, length(res$group_prior_list))
+})
+
+
+test_that("parse_header_scenario", {
+    # model choice
+    test_proj <- "IndSeq_SNP_model_choice"
+    test_dir <- file.path(data4test_dir(), test_proj)
+    file_name <- file.path(test_dir, "headerRF.txt")
+    header <- str_split(read_file(file_name), "\n", simplify = TRUE)
+    
+    content <- header[5:13]
+    res <- parse_header_scenario(content)
     
     expect_equal(
         res,
@@ -63,10 +221,31 @@ test_that("parse_scenario_header", {
         )
     )
     
-    res <- parse_scenario_header(content[6:13])
+    content <- header[6:13]
+    res <- parse_header_scenario(content)
     
     expect_false(res$valid)
 })
+
+
+test_that("parse_header_group_prior", {
+    # microsat
+    test_proj <- "Microsat_Sequences"
+    test_dir <- file.path(data4test_dir(), test_proj)
+    file_name <- file.path(test_dir, "header.txt")
+    header <- str_split(read_file(file_name), "\n", simplify = TRUE)
+    
+    content <- header[60:74]
+    n_group <- 2
+    current_line <- 59
+    res <- parse_header_group_prior(content, n_group, current_line)
+    
+    expect_true(res$valid)
+    expect_equal(length(res$group_prior), 2)
+    expect_equal(length(res$mss_type), 2)
+    expect_equal(res$current_line, 74)
+})
+
 
 test_that("read_statobs", {
     
