5d8c708bf0e087d6d4dc8b679c0280b7646aad06
GD <gd.dev@libertymail.net>
2021-04-30 16:30:11 +0200

    check MSS group prior

--- R-pkg/R/42_input_check.R
+++ R-pkg/R/42_input_check.R
@@ -81,3 +81,72 @@ check_header_loci_desc <- function(strng, type = "mss") {
     ## output
     return(valid)
 }
+
+#' Check header file group prior definition
+#' @keywords internal
+#' @author Ghislain Durif
+#' @description
+#' Content: see doc
+#' @param content string vector of group prior descriptions.
+#' @param type string, locus type `"M"` or `"S"`.
+check_header_group_prior <- function(content, type = "M") {
+    
+    prior_regex_list <- NULL
+    
+    prior_param_regex4 <- str_c(rep(numexp_regex(), 4), collapse = ",")
+    prior_param_regex2 <- str_c(rep(numexp_regex(), 2), collapse = ",")
+    
+    # vector of regex
+    if(type == "M") {
+        prior_regex_list <- c(
+            str_c("^MEANMU (UN|LU|GA)\\[", prior_param_regex4, "\\]$"),
+            str_c(
+                "^GAMMU GA\\[", prior_param_regex2, ",Mean_u,", 
+                numexp_regex(), "\\]$"
+            ),
+            str_c("^MEANP (UN|LU|GA)\\[", prior_param_regex4, "\\]$"),
+            str_c(
+                "^GAMP GA\\[", prior_param_regex2, ",Mean_P,", 
+                numexp_regex(), "\\]$"
+            ),
+            str_c("^MEANSNI (UN|LU|GA)\\[", prior_param_regex4, "\\]$"),
+            str_c(
+                "^GAMSNI GA\\[", prior_param_regex2, ",Mean_u_SNI,", 
+                numexp_regex(), "\\]$"
+            )
+        )
+    } else if(type == "S") {
+        prior_regex_list <- c(
+            str_c("^MEANMU (UN|LU|GA)\\[", prior_param_regex4, "\\]$"),
+            str_c(
+                "^GAMMU GA\\[", prior_param_regex2, ",Mean_u,", 
+                numexp_regex(), "\\]$"
+            ),
+            str_c("^MEANK1 (UN|LU|GA)\\[", prior_param_regex4, "\\]$"),
+            str_c(
+                "^GAMK1 GA\\[", prior_param_regex2, ",Mean_k1,", 
+                numexp_regex(), "\\]$"
+            ),
+            str_c("^MEANK2 (UN|LU|GA)\\[", prior_param_regex4, "\\]$"),
+            str_c(
+                "^GAMK2 GA\\[", prior_param_regex2, ",Mean_k2,", 
+                numexp_regex(), "\\]$"
+            ),
+            str_c("^MODEL (TN|HKY|JK|K2P) ", int_regex(), " ", numexp_regex())
+        )
+    }
+    
+    # check number of group prior
+    if(length(content) != length(prior_regex_list)) {
+        return(FALSE)
+    }
+    
+    # check all pattern
+    valid <- all(unlist(lapply(
+        1:length(content),
+        function(ind) return(str_detect(content[ind], prior_regex_list[ind]))
+    )))
+    
+    ## output
+    return(valid)
+}
--- R-pkg/tests/testthat/test-42_input_check.R
+++ R-pkg/tests/testthat/test-42_input_check.R
@@ -43,47 +43,146 @@ test_that("check_header_cond", {
     expect_false(check_header_cond("<t2"))
 })
 
-test_that("check_header_loci_desc", {
+
+test_that("check_header_group_prior", {
     ## MSS
-    expect_true(check_header_loci_desc(
+    expect_true(check_header_group_prior(
         "Locus_xxx <A> [M] G1 2 40", type = "mss"
     ))
-    expect_true(check_header_loci_desc(
+    expect_true(check_header_group_prior(
         "Locus_yyy <M> [S] G2 1000", type = "mss"
     ))
     
-    expect_false(check_header_loci_desc(
+    expect_false(check_header_group_prior(
         "Locus_xxx <A> [M G1 2 40", type = "mss"
     ))
-    expect_false(check_header_loci_desc(
+    expect_false(check_header_group_prior(
         "<A> [M] G1 2 40", type = "mss"
     ))
-    expect_false(check_header_loci_desc(
+    expect_false(check_header_group_prior(
         "Locus_xxx <A> [M] G1 2", type = "mss"
     ))
-    expect_false(check_header_loci_desc(
+    expect_false(check_header_group_prior(
         "Locus_yyy <M [S] G2 1000", type = "mss"
     ))
-    expect_false(check_header_loci_desc(
+    expect_false(check_header_group_prior(
         "Locus_yyy <M [S] G2 1000 3000", type = "mss"
     ))
     
     ## SNP
-    expect_true(check_header_loci_desc(
+    expect_true(check_header_group_prior(
         "5000 <A> G1 from 1", type = "snp"
     ))
-    expect_true(check_header_loci_desc(
+    expect_true(check_header_group_prior(
         "5000 <X> G2 from 10", type = "snp"
     ))
     
-    expect_false(check_header_loci_desc(
+    expect_false(check_header_group_prior(
         "<X> G2 from 10", type = "snp"
     ))
-    expect_false(check_header_loci_desc(
+    expect_false(check_header_group_prior(
         "5000 <X> G2 from ", type = "snp"
     ))
-    expect_false(check_header_loci_desc(
+    expect_false(check_header_group_prior(
         "5000 <X> G from 10", type = "snp"
     ))
+})
+
+
+test_that("check_header_group_prior", {
+    ## MSS
+    content <- c(
+        "MEANMU UN[1.00E-004,1.00E-3,0.0005,2]",
+        "GAMMU GA[1.00E-005,1.00E-002,Mean_u,2]",
+        "MEANP UN[1.00E-001,3.00E-001,0.22,2]",
+        "GAMP GA[1.00E-002,9.00E-001,Mean_P,2]",
+        "MEANSNI LU[1.00E-008,1.00E-005,1.00E-007,2]",
+        "GAMSNI GA[1.00E-009,1.00E-004,Mean_u_SNI,2]"
+    )
+    expect_true(check_header_group_prior(content, type = "M"))
+    
+    content <- c(
+        "MEANMU UN[1.00E-004,1.00E-3,0.0005,2]",
+        "GAMMU GA[1.00E-005,1.00E-002,Mean_u,2]",
+        "MEANP UN[1.00E-001,3.00E-001,0.22,2]",
+        "GAMP GA[1.00E-002,9.00E-001,Mean_P,2]"
+    )
+    expect_false(check_header_group_prior(content, type = "M"))
+    
+    content <- c(
+        "MEANMU UN[1.00E-004,1.00E-3,0.0005,2",
+        "GAMMU GA[1.00E-005,1.00E-002,Mean_u,2]",
+        "MEANP UN[1.00E-001,3.00E-001,0.22,2]",
+        "GAMP GA[1.00E-002,9.00E-001,Mean_P,2]",
+        "MEANSNI LU[1.00E-008,1.00E-005,1.00E-007,2]",
+        "GAMSNI GA[1.00E-009,1.00E-004,Mean_u_SNI,2]"
+    )
+    expect_false(check_header_group_prior(content, type = "M"))
+    
+    content <- c(
+        "MEANMU UN[1.00E-9,1.00E-7,5E-9,2]",
+        "GAMMU GA[1.00E-9,1.00E-6,Mean_u,2]",
+        "MEANK1 UN[0.050,20,10,2]",
+        "GAMK1 GA[0.050,20,Mean_k1,2]",
+        "MEANK2 UN[0.050,20,10,2]",
+        "GAMK2 GA[0.050,20,Mean_k2,2]",
+        "MODEL TN 10 2.00"
+    )
+    expect_false(check_header_group_prior(content, type = "M"))
+    
+    
+    ## SNP
+    content <- c(
+        "MEANMU UN[1.00E-9,1.00E-7,5E-9,2]",
+        "GAMMU GA[1.00E-9,1.00E-6,Mean_u,2]",
+        "MEANK1 UN[0.050,20,10,2]",
+        "GAMK1 GA[0.050,20,Mean_k1,2]",
+        "MEANK2 UN[0.050,20,10,2]",
+        "GAMK2 GA[0.050,20,Mean_k2,2]",
+        "MODEL TN 10 2.00"
+    )
+    expect_true(check_header_group_prior(content, type = "S"))
+    
+    content <- c(
+        "MEANMU UN[1.00E-9,1.00E-7,5E-9,2]",
+        "GAMMU GA[1.00E-9,1.00E-6,Mean_u,2]",
+        "MEANK1 UN[0.050,20,10,2]",
+        "GAMK1 GA[0.050,20,Mean_k1,2]",
+        "MEANK2 UN[0.050,20,10,2]",
+        "GAMK2 GA[0.050,20,Mean_k2,2]",
+        "MODEL TN 10.99 2.00"
+    )
+    expect_false(check_header_group_prior(content, type = "S"))
+    
+    content <- c(
+        "MEANMU UN[1.00E-9,1.00E-7,5E-9,2]",
+        "GAMMU GA[1.00E-9,1.00E-6,Mean_u,2]",
+        "MEANK2 UN[0.050,20,10,2]",
+        "GAMK2 GA[0.050,20,Mean_k2,2]",
+        "MODEL TN 10 2.00"
+    )
+    expect_false(check_header_group_prior(content, type = "S"))
+    
+    content <- c(
+        "MEANMU UN[1.00E-9,1.00E-7,5E-9,2]",
+        "GAMMU GA[1.00E-9,1.00E-6,Mean_u,2]",
+        "MEANK1 UN[0.050,20,10,2]",
+        "GAMK1 GA[0.050,20,0,2]",
+        "MEANK2 UN[0.050,20,10,2]",
+        "GAMK2 GA[0.050,20,Mean_k2,2]",
+        "MODEL TN 10 2.00"
+    )
+    expect_false(check_header_group_prior(content, type = "S"))
+    
+    
+    content <- c(
+        "MEANMU UN[1.00E-004,1.00E-3,0.0005,2]",
+        "GAMMU GA[1.00E-005,1.00E-002,Mean_u,2]",
+        "MEANP UN[1.00E-001,3.00E-001,0.22,2]",
+        "GAMP GA[1.00E-002,9.00E-001,Mean_P,2]",
+        "MEANSNI LU[1.00E-008,1.00E-005,1.00E-007,2]",
+        "GAMSNI GA[1.00E-009,1.00E-004,Mean_u_SNI,2]"
+    )
+    expect_false(check_header_group_prior(content, type = "S"))
 })
 
