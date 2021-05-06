6222eb63a5b0321d4a1e726cfe708662eef05806
GD <gd.dev@libertymail.net>
2021-05-06 18:19:46 +0200

    move data check function to another file + minor modification in data check

--- R-pkg/R/62_analysis_project_io.R
+++ R-pkg/R/62_analysis_project_io.R
@@ -337,6 +337,35 @@ check_proj_file <- function(proj_dir, locus_type = "snp") {
     return(out)
 }
 
+#' Check data file
+#' @keywords internal
+#' @author Ghislain Durif
+#' @param data_file string, data file name.
+#' @param data_dir string, path to directory where data file is stored.
+#' @param locus_type string, locus type `"mss"` or `"snp"`.
+#' @param seq_mode string, `"indseq"` or `"poolseq"`.
+check_data_file <- function(data_file, data_dir, locus_type, seq_mode) {
+    # output
+    out <- NULL
+    # ## debugging
+    # logging("data_file = ", data_file)
+    ## mss locus
+    if(locus_type == "mss") {
+        out <- read_mss_data(data_file, data_dir)
+        ## snp locus / indseq
+    } else if((locus_type == "snp") && (seq_mode == "indseq")) {
+        out <- read_indseq_snp_data(data_file, data_dir)
+        ## snp locus / poolseq
+    } else if((locus_type == "snp") && (seq_mode == "poolseq")) {
+        out <- read_poolseq_snp_data(data_file, data_dir)
+    } else {
+        stop("Issue with input arguments")
+    }
+    
+    ## output
+    return(out)
+}
+
 #' Format data info for user output
 #' @keywords internal
 #' @author Ghislain Durif
