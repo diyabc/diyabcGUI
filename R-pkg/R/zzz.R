07b85293146167af99efad952ba65a0984749650
GD <gd.dev@libertymail.net>
2021-08-24 17:04:04 +0200

    raise warning when loading package if missing binary file

--- R-pkg/R/zzz.R
+++ R-pkg/R/zzz.R
@@ -3,6 +3,8 @@ env <- new.env(parent = emptyenv())
 .onLoad <- function(libname, pkgname) {
     # set up options
     set_diyabcGUI_options(ncore = as.integer(0.75 * parallel::detectCores()))
+    # setup package global environment
+    init_diyabc_env()
     # check if binary files are available
     diyabc_bin <- tryCatch(
         find_bin("diyabc"),
@@ -19,25 +21,4 @@ env <- new.env(parent = emptyenv())
             "'diyabcGUI::dl_all_latest_bin()'"
         )
     }
-    # setup package global environment
-    init_diyabc_env()
 }
-
-# .onAttach <- function() {
-#     # check if binary files are available
-#     diyabc_bin <- tryCatch(
-#         find_bin("diyabc"),
-#         error = function(e) return(e)
-#     )
-#     abcranger_bin <- tryCatch(
-#         find_bin("abcranger"),
-#         error = function(e) return(e)
-#     )
-#     if("error" %in% class(diyabc_bin) | "error" %in% class(abcranger_bin)) {
-#         packageStartupMessage(
-#             "Warning: ",
-#             "Missing binary file(s), please run ",
-#             "'diyabcGUI::dl_all_latest_bin()'"
-#         )
-#     }
-# }
